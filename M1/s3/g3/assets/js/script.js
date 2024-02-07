0b87b8d23030314519eccaf8b60184bb0ac50a47
Federico Carcone <123782943+Fedec99@users.noreply.github.com>
2024-02-07 16:59:08 +0100

    task

---
+++ M1/s3/g3/assets/js/script.js
@@ -0,0 +1,31 @@
+function addTask() {
+    let taskInput = document.getElementById('taskInput');
+    let taskName = taskInput.value.trim();
+
+    if (taskName !== '') {
+    
+    let taskList = document.getElementById('taskList');
+    
+    let li = document.createElement('li');
+        li.textContent = taskName;
+        li.addEventListener('click', function() {
+        li.classList.toggle('task-completed');
+    });
+    
+    let deleteBtn = document.createElement('button');
+        deleteBtn.textContent = 'Delete';
+        deleteBtn.classList.add('delete-btn');
+        deleteBtn.addEventListener('click', function() {
+        taskList.removeChild(li);
+    });
+    
+    li.appendChild(deleteBtn);
+    taskList.appendChild(li);
+    taskInput.value = '';
+    }
+    document.getElementById('taskInput').addEventListener('keypress', function(event) {
+        if (event.key === 'Enter') {
+            addTask();
+        }
+      })
+    }
