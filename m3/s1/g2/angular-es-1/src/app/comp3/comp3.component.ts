7774b10cf2ca272209d00c9885508a37c96bdc62
Federico Carcone <123782943+Fedec99@users.noreply.github.com>
2024-03-19 14:50:58 +0100

    angular 1

---
+++ m3/s1/g2/angular-es-1/src/app/comp3/comp3.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-comp3',
+  templateUrl: './comp3.component.html',
+  styleUrl: './comp3.component.scss'
+})
+export class Comp3Component implements OnInit{
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+
+}
