6289116a5c983b180b6f32a76a601a13cb594b1e
Federico Carcone <123782943+Fedec99@users.noreply.github.com>
2024-03-29 13:19:39 +0100

    secondo committino

--- m3/s2/progetto g5/to-do/src/app/app-routing.module.ts
+++ m3/s2/progetto g5/to-do/src/app/app-routing.module.ts
@@ -1,13 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { TodoListComponent } from './component/todo-list/todo-list.component';
+import { HomeComponent } from './component/todo-list/todo-list.component';
 import { CompletedTodoListComponent } from './component/completed-todo-list/completed-todo-list.component';
+import { IncompleteTodoListComponent } from './component/incomplete-todo-list/incomplete-todo-list.component';
 import { UserListComponent } from './component/user-list/user-list.component';
 
 const routes: Routes = [
-  { path: '', component: TodoListComponent },
-  { path: 'completed', component: CompletedTodoListComponent },
-  { path: 'users', component: UserListComponent }
+  {
+    path:'',
+    component: HomeComponent,
+    title: "Home"
+  },
+  {
+    path:'complete',
+    component: CompletedTodoListComponent
+  },
+  {
+    path:'Incomplete',
+    component: IncompleteTodoListComponent
+  },
+  {
+    path:'user',
+    component: UserListComponent
+  },
 ];
 
 @NgModule({
