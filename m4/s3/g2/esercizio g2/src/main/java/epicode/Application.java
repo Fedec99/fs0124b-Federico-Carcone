ef64a1715aa3cbf0e7579b4fed5249646c779ed8
Federico <fedec.1411@gmail.com>
2024-04-30 09:37:53 +0200

    committino

---
+++ m4/s3/g2/esercizio g2/src/main/java/epicode/Application.java
@@ -0,0 +1,43 @@
+package epicode;
+
+import epicode.dao.EventsDAO;
+import epicode.entities.Event;
+import epicode.entities.TipoEvento;
+
+import java.time.LocalDate;
+import java.util.Locale;
+import java.util.Random;
+
+public class Application {
+    private static Object Persistence;
+    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("u4d12");
+
+    public static void main(String[] args) {
+        EntityManager em = emf.createEntityManager();
+        Faker faker = new Faker(Locale.ITALY);
+        EventsDAO eventsDAO = new EventsDAO(em);
+        Random rndm = new Random();
+
+        for (int i = 0; i < 20; i++) {
+            eventsDAO.save(new Event(
+                    faker.chuckNorris().fact(),
+                    LocalDate.of(rndm.nextInt(2023, 2025),
+                            rndm.nextInt(1, 13),
+                            rndm.nextInt(1, 29)),
+                    faker.lorem().fixedString(50),
+                    rndm.nextInt(1, 3) == 1 ? TipoEvento.PRIVATO : TipoEvento.PUBBLICO,
+                    rndm.nextInt(1, 1000)));
+        }
+
+        Event found = eventsDAO.getById(6);
+        if (found != null)
+            System.out.println(found);
+        else System.out.println("Elemento non trovato");
+
+
+        eventsDAO.delete(5);
+
+        em.close();
+        emf.close();
+   }
+}
